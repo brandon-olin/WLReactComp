f68f92cf498416e82a38984998f26a362e28de0d
brandon-olin <olin.brandon@gmail.com>
2020-12-17 10:42:30 -0700

    Navbar component created and rendering as in the comp. Still need to add styling for different screen sizes

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Navbar from './components/Navbar';
+import 'fontsource-roboto';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="HeroSection">
+        <Navbar />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Logo from '../images/Logo.png'
+
+const Navbar = () => {
+  return (
+    <header>
+      <div className="Links">
+        <a href="/">STORE</a>
+        <a href="/">TUTORIALS</a>
+      </div>
+      <div className="CenterLogo">
+        <img src={Logo} id="NavBarLogo" />
+      </div>
+      <div className="Links">
+        <a href="/">BLOG</a>
+        <a href="/">CONTACT</a>
+      </div>
+    </header>
+  )
+}
+
+export default Navbar;
