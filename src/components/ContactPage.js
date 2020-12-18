023933f6467cdc2374351614c491cbea0737952e
brandon-olin <olin.brandon@gmail.com>
2020-12-17 17:00:24 -0700

    Media queries added to resize images and font sizes on different sized devices. Also conditionally render a mobile navbar or desktop navbar depending on screen size.

--- src/components/ContactPage.js
+++ src/components/ContactPage.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ContactPageNavbar from './ContactPageNavbar';
+import MobileNavbar from './MobileNavbar';
 import ContactForm from './ContactForm';
 
 const ContactPage = () => {
   return (
     <div className="HomePage">
+      <MobileNavbar />
       <ContactPageNavbar />
       <div className="ContactPage">
         <ContactForm />
